9f689465fde2a8a4b8b64319a7507d57beb6d99e
Filip <el.filberto@gmail.com>
2023-01-02 14:09:50 +0100

    Day 1 solved

--- 2022/Day1/day1/src/main.rs
+++ 2022/Day1/day1/src/main.rs
@@ -3,7 +3,28 @@ use std::fs;
 fn main() {
     
     let food = fs::read_to_string("elves.txt").expect("Should read file");
+    let mut cal = 0;
+    let mut max = 0;
     for line in food.lines() {
-        println!("{}", line);
+
+        if line.is_empty() {
+            if cal > max {
+                max = cal;
+            }
+            cal = 0;
+            continue;
+        }
+
+        match line.parse::<i64>() {
+            Ok(n) => {
+                cal += n;
+            },
+            Err(e) => {
+                println!("Error {}", e);
+                continue;
+            }
+        };
     }
+    println!("Max: {}", max);
 }
+
