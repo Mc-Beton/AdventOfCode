95326a6a63fedb3e8d6be1b0df80964d0820a62c
Filip <el.filberto@gmail.com>
2023-01-18 12:04:44 +0100

    Add solution to day6 part 2

--- 2022/Day6/day6/src/main.rs
+++ 2022/Day6/day6/src/main.rs
@@ -4,16 +4,18 @@ use std::collections::HashSet;
 fn main() {
     let packet_list = fs::read_to_string("packet_list.txt").unwrap();
 
-    fn find_first_marker(input: String) -> usize {
+    fn find_first_marker(input: &str, size: usize) -> usize {
         input.chars().collect::<Vec<char>>()
-            .windows(4)
+            .windows(size)
             .map(|chars| chars.iter().collect::<HashSet<&char>>())
             .enumerate()
-            .find(|(_, set)| set.len() == 4)
-            .map(|(index, _)| index + 4)
+            .find(|(_, set)| set.len() == size)
+            .map(|(index, _)| index + size)
             .unwrap()
     }
 
-    let answer = find_first_marker(packet_list);
-    println!("{}", answer);
+    let answer1 = find_first_marker(&packet_list, 4);
+    let answer2 = find_first_marker(&packet_list, 14);
+    println!("{}", answer1);
+    println!("{}", answer2);
 }
