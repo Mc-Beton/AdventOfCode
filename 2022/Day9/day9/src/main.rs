599d258fbca4b57370ce28700ed64cd58887f247
Filip <el.filberto@gmail.com>
2023-01-23 13:20:35 +0100

    Add impl for structs

--- 2022/Day9/day9/src/main.rs
+++ 2022/Day9/day9/src/main.rs
@@ -1,9 +1,21 @@
 use std::fs;
+use itertools::Itertools;
+use std::collections::HashSet;
 use crate::Directions::{Left, Right, Up, Down};
 
 fn main() {
-    let rope_move = fs::read_to_string("move_list.txt").unwrap();
-    
+    let moves = fs::read_to_string("move_list.txt").unwrap();
+
+    let pos_zero = Position {x: 0, y: 0};
+
+    let move_list: Vec<Move> = moves.split('\n')
+        .map(|line| line.split_whitespace().next_tuple::<(_, _)>().unwrap())
+        .map(Move::from).collect();
+}
+
+struct Position {
+    x: isize,
+    y: isize,
 }
 
 struct Move {
@@ -11,6 +23,7 @@ struct Move {
     steps: usize,
 }
 
+#[derive(Copy, Debug, Clone)]
 enum Directions {
     Right,
     Left,
@@ -18,9 +31,48 @@ enum Directions {
     Down,
 }
 
+impl Move {
+    fn do_move(&self, pos: Position) -> Position {
+        match self.direction {
+            Right => pos.move_right(self.steps),
+            Left => pos.move_left(self.steps),
+            Up => pos.move_up(self.steps),
+            Down => pos.move_down(self.steps),
+            _ => unimplemented!()
+        }
+    }
+
+    fn move_once(&self) -> Self {
+        Move {
+            direction: self.direction,
+            steps: 1,
+        }
+    }
+}
+
+impl Position {
+    fn new_pos(x: isize, y: isize) -> Self {Position {x, y}}
+
+    fn move_right(&self, steps: usize) -> Position {
+        Self::new_pos(self.x + steps as isize, self.y)
+    }
+
+    fn move_left(&self, steps: usize) -> Position {
+        Self::new_pos(self.x - steps as isize, self.y)
+    }
+
+    fn move_up(&self, steps: usize) -> Position {
+        Self::new_pos(self.x, self.y + steps as isize)
+    }
+
+    fn move_down(&self, steps: usize) -> Position {
+        Self::new_pos(self.x, self.y - steps as isize)
+    }
+}
+
 impl From<(&str, &str)> for Move {
-    fn from(lines: (&str, &str)) -> Self {
-        let (dir, steps) = lines;
+    fn from(line: (&str, &str)) -> Self {
+        let (dir, steps) = line;
         Move {
             direction: match dir {
                 "R" => Right,
