b4cf393214b6ccc775cbd506dd20a570863aa8f9
Filip <el.filberto@gmail.com>
2023-01-23 14:59:04 +0100

    Add solution to day9

--- 2022/Day9/day9/src/main.rs
+++ 2022/Day9/day9/src/main.rs
@@ -1,88 +1,38 @@
 use std::fs;
 use itertools::Itertools;
 use std::collections::HashSet;
-use crate::Directions::{Left, Right, Up, Down};
+use day9::{Position, Move};
 
 fn main() {
     let moves = fs::read_to_string("move_list.txt").unwrap();
 
-    let pos_zero = Position {x: 0, y: 0};
-
     let move_list: Vec<Move> = moves.split('\n')
         .map(|line| line.split_whitespace().next_tuple::<(_, _)>().unwrap())
         .map(Move::from).collect();
-}
 
-struct Position {
-    x: isize,
-    y: isize,
-}
+    let tail_moves = {
+        let head = Position::new_pos(0, 0);
+        let tail = Position::new_pos(0, 0);
+        let mut head_list = Vec::new();
+        let mut tail_list = Vec::new();
+        head_list.push(head);
+        tail_list.push(tail);
+        move_list.iter().for_each(|m| {
+            for _ in 0..m.steps {
+                head_list.push(m.move_once().do_move(*head_list.last().unwrap()));
+                tail_list.push(tail_list.last().unwrap().follow(head_list.last().unwrap()));
+            }
+        });
+        tail_list
+    };
 
-struct Move {
-    direction: Directions,
-    steps: usize,
-}
-
-#[derive(Copy, Debug, Clone)]
-enum Directions {
-    Right,
-    Left,
-    Up,
-    Down,
-}
-
-impl Move {
-    fn do_move(&self, pos: Position) -> Position {
-        match self.direction {
-            Right => pos.move_right(self.steps),
-            Left => pos.move_left(self.steps),
-            Up => pos.move_up(self.steps),
-            Down => pos.move_down(self.steps),
-            _ => unimplemented!()
-        }
+    let mut solution1 = HashSet::new();
+    for i in tail_moves {
+        solution1.insert(i);
     }
 
-    fn move_once(&self) -> Self {
-        Move {
-            direction: self.direction,
-            steps: 1,
-        }
-    }
+    println!("{}", solution1.len());
 }
 
-impl Position {
-    fn new_pos(x: isize, y: isize) -> Self {Position {x, y}}
 
-    fn move_right(&self, steps: usize) -> Position {
-        Self::new_pos(self.x + steps as isize, self.y)
-    }
-
-    fn move_left(&self, steps: usize) -> Position {
-        Self::new_pos(self.x - steps as isize, self.y)
-    }
-
-    fn move_up(&self, steps: usize) -> Position {
-        Self::new_pos(self.x, self.y + steps as isize)
-    }
-
-    fn move_down(&self, steps: usize) -> Position {
-        Self::new_pos(self.x, self.y - steps as isize)
-    }
-}
-
-impl From<(&str, &str)> for Move {
-    fn from(line: (&str, &str)) -> Self {
-        let (dir, steps) = line;
-        Move {
-            direction: match dir {
-                "R" => Right,
-                "L" => Left,
-                "U" => Up,
-                "D" => Down,
-                _ => unimplemented!()
-            },
-            steps: steps.parse::<usize>().unwrap(),
-        }
-    }
-}
 
