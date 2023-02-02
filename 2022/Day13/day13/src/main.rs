32d5ae2d37cdfa77dcae48743279138a94be9296
Filip <el.filberto@gmail.com>
2023-02-02 16:13:41 +0100

    Add solution to day 13

--- 2022/Day13/day13/src/main.rs
+++ 2022/Day13/day13/src/main.rs
@@ -1,14 +1,25 @@
 use std::fs;
-use itertools::Itertools;
+use itertools::{Itertools, EitherOrBoth};
 use crate::Digit::{Value, List};
 
 fn main() {
     
     let signal = fs::read_to_string("signal.txt").unwrap();
-    let mut sig_list = signal.split('\n')
+    let sig_list = signal.split('\n')
         .filter(|line| !line.trim().is_empty())
         .map(Digit::from)
         .collect_vec();
+
+    let solution1 = sig_list.iter()
+        .tuples::<(_, _)>().enumerate()
+        .map(|(i, (l, r))| {
+            (i+1,
+            l.compare(r))
+        }).filter(|(i, c)| matches!(c, Comp::Valid))
+        .map(|(i, _)| i)
+        .sum::<usize>();
+
+    println!("{}", solution1);
 
 }
 
@@ -25,7 +36,53 @@ impl Digit {
             List(v) => Digit::List(v.clone()),
         }
     }
+
+    fn compare(&self, r_side: &Digit) -> Comp {
+
+        let c = match (self, r_side) {
+            (Value(l), Value(r)) => match (l,r) {
+                (l,r) if l < r => Comp::Valid,
+                (l,r) if l > r => Comp::Wrong,
+                (l,r) if l == r => Comp::Equal,
+                _ => unimplemented!(),
+            },
+            (Value(_), List(_)) => self.to_compare().compare(r_side),
+            (List(_), Value(_)) => self.compare(&r_side.to_compare()),
+            (List(l), List(r)) => {
+                let double_iter = l.iter().zip_longest(r.iter());
+
+                let mut result = false;
+                for pair in double_iter {
+                    match pair {
+                        EitherOrBoth::Both(l,r) => {
+                            let c = l.compare(r);
+                            match c {
+                                Comp::Equal => {},
+                                Comp::Wrong => return c,
+                                Comp::Valid => return c,
+                            }
+                        }
+                        EitherOrBoth::Right(_) => {
+                            result = true;
+                        },
+                        EitherOrBoth::Left(_) => {
+                            if !result {
+                                return Comp::Wrong
+                            }
+                        }
+                    }
+                }
+                if result {
+                    Comp::Valid
+                } else {
+                    Comp::Equal
+                }
+            }, 
+        };
+        c
+    }
 }
+
 
 impl From<&str> for Digit {
     fn from(line: &str) -> Self {
@@ -35,7 +92,6 @@ impl From<&str> for Digit {
 
         for c in line.chars() {
             match c {
-                num => buffer.push(num),
                 '[' => {
                     let new_list = List(vec![]);
                     open_vec.push(new_list);
@@ -69,8 +125,15 @@ impl From<&str> for Digit {
                         buffer.clear();
                     }
                 },
+                num => buffer.push(num),
             }
         };
         unimplemented!();
     }
+}
+
+enum Comp {
+    Valid,
+    Wrong,
+    Equal,
 }
