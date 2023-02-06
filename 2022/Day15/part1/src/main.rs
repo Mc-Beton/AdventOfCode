f4b88a49076ae7675a74aa4d69c793731719366b
Filip <el.filberto@gmail.com>
2023-02-06 17:42:39 +0100

    Add solution

--- 2022/Day15/part1/src/main.rs
+++ 2022/Day15/part1/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::collections::HashSet;
+use std::iter::Iterator;
 
 fn main() {
     let sensors = fs::read_to_string("sensors.txt").unwrap();
@@ -13,7 +14,30 @@ fn main() {
         sens_map.insert(s.beac);
     }
 
-    println!("{:?}", sens_map);
+    let mut sig_map = sens_map.clone();
+
+    for i in sensor_list {
+        let list = i.get_sensor_area(2000000);
+        for j in list {
+            sig_map.insert(j);
+        }
+    }
+
+    let solution1 = get_row_length(sig_map, sens_map, 2000000);
+    
+    println!("{}", solution1);
+}
+
+fn get_row_length(sig: HashSet<(isize, isize)>, set: HashSet<(isize, isize)>, row: isize) -> usize {
+    let map = sig.iter()
+        .filter(|coor| coor.1 == row)
+        .count();
+
+    let beacons = set.iter()
+        .filter(|coor| coor.1 == row)
+        .count();
+
+    map - beacons
 }
 
 #[derive(Debug)]
@@ -30,5 +54,25 @@ impl From<&str> for Sensor {
         let bx = coors[1].trim().split(' ').nth(4).unwrap().split('=').nth(1).unwrap().replace(",", "").parse::<isize>().unwrap();
         let by = coors[1].trim().split(' ').last().unwrap().split('=').nth(1).unwrap().parse::<isize>().unwrap();
         Sensor {sens: (sx, sy), beac: (bx, by)}
+    }
+}
+
+impl Sensor {
+
+    fn get_distance_to_beacon(&self) -> isize {
+        (self.sens.0 - self.beac.0).abs() + (self.sens.1 - self.beac.1).abs()
+    }
+
+    fn get_sensor_area(&self, row: isize) -> Vec<(isize, isize)> {
+        let dist = self.get_distance_to_beacon();
+        let mut area = Vec::new();
+
+        if ((self.sens.1 - dist)..self.sens.1).contains(&row) || ((self.sens.1)..(self.sens.1 + dist)).contains(&row) {
+            let dif = (dist - (row - self.sens.1).abs()).abs();
+            for i in (-dif)..(dif+1) {
+                area.push((self.sens.0 + i, row));
+            }
+        }
+        area
     }
 }
