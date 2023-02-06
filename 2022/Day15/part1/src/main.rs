539e1e6f019cef49ebbf9596d9b21a6d7e26988e
Filip <el.filberto@gmail.com>
2023-02-06 14:42:10 +0100

    change read file method

--- 2022/Day15/part1/src/main.rs
+++ 2022/Day15/part1/src/main.rs
@@ -3,21 +3,9 @@ use std::collections::HashSet;
 
 fn main() {
     let sensors = fs::read_to_string("sensors.txt").unwrap();
-
-    let re = Regex::new(r"Sensor at x=(\d+), y=(\d+): closest beacon is at x=(\d+), y=(\d+)").unwrap();
-
-    let mut sensor_list = Vec::new();
-
-    for c in re.captures_iter(&sensors) {
-
-        let sensor = Sensor {
-            sens: (c[1].parse().unwrap(), c[2].parse().unwrap()),
-            beac: (c[3].parse().unwrap(), c[4].parse().unwrap()),
-        };
-
-        println!("{:?}", sensor);
-        sensor_list.push(sensor); 
-    };
+    let sensor_list: Vec<Sensor> = sensors.split('\n')
+        .map(Sensor::from)
+        .collect();
 
     let mut sens_map = HashSet::new();
     for s in &sensor_list {
@@ -25,11 +13,22 @@ fn main() {
         sens_map.insert(s.beac);
     }
 
-    //println!("{:?}", sens_map);
+    println!("{:?}", sens_map);
 }
 
 #[derive(Debug)]
 struct Sensor {
     sens: (isize, isize),
     beac: (isize, isize),
+}
+
+impl From<&str> for Sensor {
+    fn from(line: &str) -> Self {
+        let coors: Vec<&str> = line.split(':').collect();
+        let sx = coors[0].split(' ').nth(2).unwrap().split('=').nth(1).unwrap().replace(",", "").parse::<isize>().unwrap();
+        let sy = coors[0].split(' ').last().unwrap().split('=').nth(1).unwrap().parse::<isize>().unwrap();
+        let bx = coors[1].trim().split(' ').nth(4).unwrap().split('=').nth(1).unwrap().replace(",", "").parse::<isize>().unwrap();
+        let by = coors[1].trim().split(' ').last().unwrap().split('=').nth(1).unwrap().parse::<isize>().unwrap();
+        Sensor {sens: (sx, sy), beac: (bx, by)}
+    }
 }
