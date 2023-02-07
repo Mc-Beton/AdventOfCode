0921c54ed7e87092795d31fe71b782c84be73133
Filip <el.filberto@gmail.com>
2023-02-07 14:01:24 +0100

    read file

---
+++ 2022/Day12/part1/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs;
+use crate::Signature::{Start, End, Basic};
+
+fn main() {
+    let input = fs::read_to_string("map.txt").unwrap();
+
+    let rows: Vec<&str> = input.split('\n').collect();
+    let width = rows.first().unwrap().len();
+    let height = rows.len();
+
+    let chars: Vec<Location> = input.replace('\n', "").chars().map(Location::from).collect();
+}
+
+enum Signature {
+    Start,
+    End,
+    Basic,
+}
+
+struct Location {
+    signature: Signature,
+    value: char,
+}
+
+impl From<char> for Location {
+    fn from(c: char) -> Self {
+        match c {
+            'S' => Location{signature: Start, value: 'a'},
+            'E' => Location{signature: End, value: 'z'},
+            b => Location{signature: Basic, value: b},
+        }
+    }
+}
