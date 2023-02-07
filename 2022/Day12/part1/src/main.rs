4a418a4d559bb5598d69088768439bbb04f4f030
Filip <el.filberto@gmail.com>
2023-02-07 15:46:52 +0100

    Impl pathfinding, add solution

--- 2022/Day12/part1/src/main.rs
+++ 2022/Day12/part1/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use crate::Signature::{Start, End, Basic};
-use ndarray::{Array2, Ix};
+use ndarray::{Array2, Ix, Ix2};
+use pathfinding::prelude::astar;
 
 fn main() {
     let input = fs::read_to_string("map.txt").unwrap();
@@ -22,22 +23,50 @@ fn main() {
         .find(|(_, loc)| loc.find_end())
         .map(|(pos, loc)| Node {pos, loc: loc.clone()})
         .unwrap();
+
+    let solution1 = astar(&start, |n| find_neighbours(n, &map), |n| heuristic(n, &end), |n| n.loc.find_end())
+        .map(|(_, cost)| cost)
+        .unwrap();
+
+    println!("{}", solution1);
 }
 
-#[derive(Debug, Clone)]
+fn find_neighbours(n: &Node, map: &Array2<Location>) -> Vec<(Node, usize)> {
+    let up = (n.pos.0 as isize + 1, n.pos.1 as isize);
+    let down = (n.pos.0 as isize - 1, n.pos.1 as isize);
+    let left = (n.pos.0 as isize, n.pos.1 as isize + 1);
+    let right = (n.pos.0 as isize, n.pos.1 as isize - 1);
+
+    let nodes: Vec<(Node, usize)> = vec![up, down, left, right].iter()
+        .map(|(x, y)| (*x as Ix, *y as Ix))
+        .filter_map(|c| map.get(c).map(|loc| {
+            Node {pos: c, loc: loc.clone() }
+        }))
+        .filter(|s| (s.loc.get_value_as_int() as isize - n.loc.get_value_as_int() as isize) <= 1)
+        .map(|n| (n, 1))
+        .collect();
+
+    nodes
+}
+
+fn heuristic(n1: &Node, n2: &Node) -> usize {
+    (((n2.pos.0 as isize - n1.pos.0 as isize).pow(2) + (n2.pos.1 as isize - n1.pos.1 as isize).pow(2)) as f64).sqrt() as usize
+}
+
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
 struct Node {
     pos: (Ix, Ix),
     loc: Location, 
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
 enum Signature {
     Start,
     End,
     Basic,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
 struct Location {
     signature: Signature,
     value: char,
